40c530a41fd58be48551bbad694e30b62bbe2f62
Patrick Snowden <patrickbsnowden@outlook.com>
2022-08-02 01:57:35 -0700

    added more projects at bottom of project page

--- components/Project.js
+++ components/Project.js
@@ -3,6 +3,7 @@ import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 import { SiJavascript, SiCss3, SiHtml5, SiTailwindcss } from "react-icons/si";
 import Head from "next/head";
 import { render } from "react-dom";
+import Card from "./Card";
 
 export default function Project({ projects }) {
      const router = useRouter();
@@ -46,7 +47,6 @@ export default function Project({ projects }) {
                          blurbConclusion,
                          githubLink,
                          liveLink,
-                         id,
                          image,
                          name,
                          techUsed,
@@ -54,7 +54,7 @@ export default function Project({ projects }) {
                     } = project;
                     return (
                          <section
-                              key={id}
+                              key={project.id}
                               className="my-10 md:my-20 w-full md:w-[880px]"
                          >
                               <div className="w-full md:w-[880px] border-b border-dashed pb-5 mb-10">
@@ -75,37 +75,58 @@ export default function Project({ projects }) {
                                         ))}
                                    </ul>
                               </div>
-                              <div className="mb-10">
-                                   <img
-                                        src={image}
-                                        alt=""
-                                        className="mx-auto"
-                                   />
-                              </div>
-                              <div className="w-full md:w-[800px] mx-auto">
-                                   <p className="mb-3">{blurb}</p>
-                                   <p className="mb-3">{blurbConclusion}</p>
-                                   <div className="flex item-center justify-start">
-                                        <a
-                                             href={project.githubLink}
-                                             target="_blank"
-                                             rel="noreferrer"
-                                             className="flex items-center justify-center py-0.5 px-1 rounded mr-1 text-sm bg-[#2A2A2A] hover:bg-[#424242] tracking-widest"
-                                        >
-                                             Github{" "}
-                                             <FaGithub className="ml-1" />
-                                        </a>
-                                        <a
-                                             href={project.liveLink}
-                                             target="_blank"
-                                             rel="noreferrer"
-                                             className="flex items-center justify-center py-0.5 px-1 rounded text-sm bg-[#2A2A2A] hover:bg-[#424242] tracking-widest"
-                                        >
-                                             Live{" "}
-                                             <FaExternalLinkAlt className="ml-1" />
-                                        </a>
+                              <section className="border-b border-dashed pb-5">
+                                   <div className="mb-10">
+                                        <img
+                                             src={image}
+                                             alt=""
+                                             className="mx-auto"
+                                        />
                                    </div>
-                              </div>
+                                   <div className="w-full md:w-[800px] mx-auto pb-5">
+                                        <p className="mb-3">{blurb}</p>
+                                        <p className="mb-3">
+                                             {blurbConclusion}
+                                        </p>
+                                        <div className="flex item-center justify-start">
+                                             <a
+                                                  href={project.githubLink}
+                                                  target="_blank"
+                                                  rel="noreferrer"
+                                                  className="flex items-center justify-center py-0.5 px-1 rounded mr-1 text-sm bg-[#2A2A2A] hover:bg-[#424242] tracking-widest"
+                                             >
+                                                  Github{" "}
+                                                  <FaGithub className="ml-1" />
+                                             </a>
+                                             <a
+                                                  href={project.liveLink}
+                                                  target="_blank"
+                                                  rel="noreferrer"
+                                                  className="flex items-center justify-center py-0.5 px-1 rounded text-sm bg-[#2A2A2A] hover:bg-[#424242] tracking-widest"
+                                             >
+                                                  Live{" "}
+                                                  <FaExternalLinkAlt className="ml-1" />
+                                             </a>
+                                        </div>
+                                   </div>
+                              </section>
+                              <section className="mt-10">
+                                   <h2 className="text-3xl mb-5">
+                                        More Projects
+                                   </h2>
+                                   <div className="w-full flex items-center justify-around flex-wrap gap-2">
+                                        {projects.repo
+                                             .filter(
+                                                  (project) =>
+                                                       project.name
+                                                            .split(" ")
+                                                            .join("-") !== id
+                                             )
+                                             .map((project) => (
+                                                  <Card project={project} />
+                                             ))}
+                                   </div>
+                              </section>
                          </section>
                     );
                })}
